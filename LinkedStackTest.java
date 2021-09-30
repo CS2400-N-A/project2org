6feffd098b037eec30a7649ff7fd10fc90fb55a9
Carlos Castrillo <cjcastrillo@cpp.edu>
2021-09-30 13:34:57 -0700

    Added empty test method

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -1,5 +1,10 @@
 public class LinkedStackTest {
     public static void main(String[] args) {
-        
+        StackInterface<String> stackOne = new LinkedStack<String>();
+        testConvertToPostFix(stackOne);
+    }
+
+    private static void testConvertToPostFix(StackInterface<String> stack){
+
     }
 }
