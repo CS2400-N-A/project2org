ed5e8a39adcb2043dc5dcb0db31dda28450189de
Cindy Lu <cindylu@cpp.edu>
2021-10-18 21:52:24 -0700

    Removed test code

--- ResizableArrayStackTest.java
+++ ResizableArrayStackTest.java
@@ -17,32 +17,26 @@ public class ResizableArrayStackTest {
                 switch (next) {
                     case 'a':{
                         vStack.push("2");
-                        System.out.println("2");
                         break;
                     }
                     case 'b':{
                         vStack.push("3");
-                        System.out.println("3");
                         break;
                     } 
                     case 'c':{
                         vStack.push("4");
-                        System.out.println("4");
                         break;                       
                     }
                     case 'd':{
                         vStack.push("5");
-                        System.out.println("5");
                         break;
                     }
                     case 'e':{
                         vStack.push("6");
-                        System.out.println("6");
                         break;
                     }
                     case 'h':{
                         vStack.push("7");
-                        System.out.println("7");
                         break;
                     }
                     default: break;
@@ -51,43 +45,36 @@ public class ResizableArrayStackTest {
             else {
                 String op2 = vStack.pop();
                 String op1 = vStack.pop();
-                System.out.println(op2);
-                System.out.println(op1);
                 switch (next) {
                     case '+': {
 
                         int num = (Integer.parseInt(op1) + Integer.parseInt(op2));
                         String result = Integer.toString(num);
                         vStack.push(result);
-                        System.out.println(op1 + "+" + op2+ "=" +num);
                         break;
                     }
                     case '-': {
                         int num = (Integer.parseInt(op1) - Integer.parseInt(op2));
                         String result = Integer.toString(num);
                         vStack.push(result);
-                        System.out.println(op1 + "-" + op2 + "=" +num);
                         break;
                     }
                     case '*': {
                         int num = (Integer.parseInt(op1) * Integer.parseInt(op2));
                         String result = Integer.toString(num);
                         vStack.push(result);
-                        System.out.println(op1 + "*" + op2+ "=" +num);
                         break;
                     }
                     case '/': {
                         int num = (Integer.parseInt(op1) / Integer.parseInt(op2));
                         String result = Integer.toString(num);
                         vStack.push(result);
-                        System.out.println(op1 + "/" + op2+ "=" +num);
                         break;
                     }
                     case '^': {
                         int num = (int)(Math.pow(Integer.parseInt(op1), Integer.parseInt(op2)));
                         String result = Integer.toString(num);
                         vStack.push(result);
-                        System.out.println(op1 + "^" + op2+ "=" +num);
                         break; 
                     }
                     default: 
